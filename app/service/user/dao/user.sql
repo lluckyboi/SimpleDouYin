3adaa5b5cb32ea93d63cdf39f0390a8a9f14636f
lluckyboi <1598273095@qq.com>
2023-01-13 22:59:15 +0800

    update:gorm部分

--- app/service/user/dao/user.sql
+++ app/service/user/dao/user.sql
@@ -1,9 +1,11 @@
 create table user
 (
-    user_id  bigint auto_increment comment 'id',
-    username varchar(255) not null,
-    password varchar(255) not null,
-    name     varchar(50)  null,
+    user_id        bigint auto_increment comment 'id',
+    username       varchar(255)     not null,
+    password       varchar(255)     not null,
+    name           varchar(50)      null,
+    follow_count   bigint default 0 not null comment '关注总数',
+    follower_count bigint default 0 not null comment '粉丝总数',
     constraint user_user_id_uindex
         unique (user_id)
 );
