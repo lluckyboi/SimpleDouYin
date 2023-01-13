42312e9e77933d739e906956e55aa5ad71352005
lluckyboi <1598273095@qq.com>
2023-01-13 19:30:33 +0800

    feat:user.sql文件

--- app/service/user/dao/user.sql
+++ app/service/user/dao/user.sql
@@ -1,34 +1,10 @@
-# create table user
-# (
-#     uid  bigint unsigned auto_increment comment 'uid'primary key,
-#     user_true_name varchar(36) default ''     not null comment '用户真名',
-#     user_nick_name varchar(36) default 'user' not null comment '昵称',
-#     sex            enum ('男', '女')            null comment '性别',
-#     user_school    varchar(36) default ''     not null comment '学校名',
-#     user_stno      varchar(36) default ''     null comment '学工号',
-#     user_role      enum ('教师', '学生')          not null comment '身份: 教师或学生',
-#     user_pnum      varchar(16) default ''     not null comment '手机号',
-#     user_password  varchar(36) default ''     not null comment '用户密码',
-#     creatTime      datetime                   null on update CURRENT_TIMESTAMP comment '创建时间',
-#     updateTime     datetime                   null on update CURRENT_TIMESTAMP comment '修改时间',
-#     primary key (uid)
-# )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-create table gzdemo.user
+create table user
 (
-    user_true_name varchar(36) default ''     not null comment '用户真名',
-    user_nick_name varchar(36) default 'user' not null comment '昵称',
-    sex            enum ('男', '女')            null comment '性别',
-    user_school    varchar(36) default ''     not null comment '学校名',
-    user_stno      varchar(36) default ''     null comment '学工号',
-    user_role      enum ('教师', '学生')          not null comment '身份: 教师或学生',
-    user_pnum      varchar(16) default ''     not null comment '手机号',
-    user_password  varchar(36) default ''     not null comment '用户密码',
-    creatTime      datetime                   null on update CURRENT_TIMESTAMP comment '创建时间',
-    updateTime     datetime                   null on update CURRENT_TIMESTAMP comment '修改时间',
-    constraint user_pnum
-        unique (user_pnum)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-alter table gzdemo.user
-    add primary key (user_pnum);
+    user_id  bigint auto_increment comment 'id',
+    username varchar(255) not null,
+    password varchar(255) not null,
+    name     varchar(50)  null,
+    constraint user_user_id_uindex
+        unique (user_id)
+);
 
