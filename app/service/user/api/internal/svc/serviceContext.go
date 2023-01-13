9bd3b966ebb23ee9af8e26de99c7dfc916b242a1
lluckyboi <1598273095@qq.com>
2023-01-13 21:00:17 +0800

    update:

--- app/service/user/api/internal/svc/serviceContext.go
+++ app/service/user/api/internal/svc/serviceContext.go
@@ -1,19 +1,33 @@
 package svc
 
 import (
+	"SimpleDouYin/app/common"
+	"SimpleDouYin/app/common/middleware"
 	"SimpleDouYin/app/service/user/api/internal/config"
-	"SimpleDouYin/app/service/user/api/internal/middleware"
+	"SimpleDouYin/app/service/user/rpc/user"
+	"github.com/go-redis/redis"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
-	JWT    rest.Middleware
+	JWT        rest.Middleware
+	Config     config.Config
+	JWTMap     *common.JWTMap
+	UserClient user.User
+	RedisDB    *redis.Client
 }
 
-func NewServiceContext(c config.Config) *ServiceContext {
+func NewServiceContext(c config.Config, JWTMap *common.JWTMap) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
-		JWT:    middleware.NewJWTMiddleware().Handle,
+		Config:     c,
+		UserClient: user.NewUser(zrpc.MustNewClient(c.UserClient)),
+		JWT:        middleware.NewJWTMiddleware(JWTMap).Handle,
+		JWTMap:     JWTMap,
+		RedisDB: redis.NewClient(&redis.Options{
+			Addr:     c.RedisDB.RHost,
+			Password: c.RedisDB.RPass,
+			DB:       0, // use default DB
+		}),
 	}
 }
