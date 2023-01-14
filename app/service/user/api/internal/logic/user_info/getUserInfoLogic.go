b49b912d1e3e9872c8f3cd7dc1b5e5fff295cf28
lluckyboi <1598273095@qq.com>
2023-01-14 13:40:04 +0800

    update:getInfo api逻辑

--- app/service/user/api/internal/logic/user_info/getUserInfoLogic.go
+++ app/service/user/api/internal/logic/user_info/getUserInfoLogic.go
@@ -1,6 +1,8 @@
 package user_info
 
 import (
+	"SimpleDouYin/app/common"
+	"SimpleDouYin/app/service/user/rpc/user"
 	"context"
 
 	"SimpleDouYin/app/service/user/api/internal/svc"
@@ -23,8 +25,33 @@ func NewGetUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUs
 	}
 }
 
-func (l *GetUserInfoLogic) GetUserInfo() (resp *types.GetUserInfoResponse, err error) {
-	// todo: add your logic here and delete this line
+func (l *GetUserInfoLogic) GetUserInfo(req *types.GetUserInfoRequest) (resp *types.GetUserInfoResponse, err error) {
+	//解析token
+	claims, err := common.ParseToken(req.Token)
+	if err != nil {
+		logx.Error(err)
+		return
+	}
 
+	//调用rpc查询
+	GRsp, err := l.svcCtx.UserClient.GetInfo(l.ctx, &user.GetInfoReq{
+		UserId:   claims.UserId,
+		TargetId: req.UserId,
+	})
+	if err != nil {
+		logx.Error(err)
+		return
+	}
+
+	//返回结果
+	resp.StatusCode = GRsp.GetStatusCode()
+	resp.StatusMsg = GRsp.GetStatusMsg()
+	resp.User = types.User{
+		Id:            req.UserId,
+		Name:          GRsp.User.Name,
+		FollowCount:   GRsp.User.FollowerCount,
+		FollowerCount: GRsp.User.FollowerCount,
+		IsFollow:      GRsp.User.IsFollow,
+	}
 	return
 }
