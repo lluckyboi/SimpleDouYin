f22beb015ffa2b7c09888737964559292ab45b06
lluckyboi <1598273095@qq.com>
2023-01-14 14:22:01 +0800

    feat:获取用户信息逻辑

--- app/service/user/rpc/internal/logic/getInfoLogic.go
+++ app/service/user/rpc/internal/logic/getInfoLogic.go
@@ -1,7 +1,12 @@
 package logic
 
 import (
+	"SimpleDouYin/app/common"
+	"SimpleDouYin/app/service/user/dao/model"
 	"context"
+	"errors"
+	"gorm.io/gorm"
+	"net/http"
 
 	"SimpleDouYin/app/service/user/rpc/internal/svc"
 	"SimpleDouYin/app/service/user/rpc/pb"
@@ -24,7 +29,39 @@ func NewGetInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetInfoLo
 }
 
 func (l *GetInfoLogic) GetInfo(in *pb.GetInfoReq) (*pb.GetInfoReps, error) {
-	//
+	resp := new(pb.GetInfoReps)
+	User := model.User{}
+	Follow := model.Follow{}
+	isf := true
 
-	return &pb.GetInfoReps{}, nil
+	//先查询目标id信息
+	db := l.svcCtx.GormDB.Where("user_id = ?", in.TargetId).First(&User)
+	if db.Error != nil {
+		resp.StatusCode = common.ErrOfServer
+		resp.StatusMsg = common.InfoErrOfServer
+		return resp, nil
+	}
+
+	//再查询是否关注
+	db = l.svcCtx.GormDB.Where("uid = ? and target_id = ?", in.UserId, in.TargetId).First(&Follow)
+	if db.Error != nil {
+		if errors.Is(db.Error, gorm.ErrRecordNotFound) {
+			isf = false
+		} else {
+			resp.StatusCode = common.ErrOfServer
+			resp.StatusMsg = common.InfoErrOfServer
+			return resp, nil
+		}
+	}
+
+	resp.StatusCode = http.StatusOK
+	resp.StatusMsg = "查询成功"
+	resp.User = &pb.UserInfo{
+		UserId:        User.UserID,
+		Name:          User.Name,
+		FollowCount:   User.FollowCount,
+		FollowerCount: User.FollowerCount,
+		IsFollow:      isf,
+	}
+	return resp, nil
 }
