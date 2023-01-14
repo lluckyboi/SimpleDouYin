0e16d2d1e56fdee8385554ec3553ae507f4593c1
lluckyboi <1598273095@qq.com>
2023-01-14 12:45:21 +0800

    update:完善rpc登录逻辑

--- app/service/user/rpc/internal/logic/loginLogic.go
+++ app/service/user/rpc/internal/logic/loginLogic.go
@@ -1,6 +1,8 @@
 package logic
 
 import (
+	"SimpleDouYin/app/common"
+	"SimpleDouYin/app/service/user/dao/model"
 	"context"
 
 	"SimpleDouYin/app/service/user/rpc/internal/svc"
@@ -23,8 +25,20 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 	}
 }
 
-func (l *LoginLogic) Login(in *pb.LoginReq) (*pb.LoginReps, error) {
-	// todo: add your logic here and delete this line
+func (l *LoginLogic) Login(in *pb.LoginReq) (resp *pb.LoginReps, error error) {
+	user := model.User{}
+	err := l.svcCtx.GormDB.Where("username = ?", in.Username).First(&user)
+	if err.Error != nil {
+		logx.Error("登录:查询用户名错误:", err)
+		return
+	}
+	if common.RSA_Encrypt([]byte(in.Password), l.svcCtx.Config.Sec.SecPub) != user.Password {
+		resp.StatusCode = 2003
+		resp.StatusMsg = "密码错误"
+	}
 
-	return &pb.LoginReps{}, nil
+	resp.StatusCode = 200
+	resp.StatusMsg = "登录成功"
+	resp.UserId = user.UserID
+	return resp, nil
 }
