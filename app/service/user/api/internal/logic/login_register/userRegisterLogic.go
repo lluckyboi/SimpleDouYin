a89cd4ea325e45cd50f87e2a0742045b99ef854b
lluckyboi <1598273095@qq.com>
2023-01-14 12:44:25 +0800

    update:完善注册逻辑

--- app/service/user/api/internal/logic/login_register/userRegisterLogic.go
+++ app/service/user/api/internal/logic/login_register/userRegisterLogic.go
@@ -1,8 +1,10 @@
 package login_register
 
 import (
+	"SimpleDouYin/app/common"
 	"SimpleDouYin/app/service/user/api/internal/svc"
 	"SimpleDouYin/app/service/user/api/internal/types"
+	"SimpleDouYin/app/service/user/dao/model"
 	"SimpleDouYin/app/service/user/rpc/user"
 	"context"
 
@@ -23,7 +25,7 @@ func NewUserRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *User
 	}
 }
 
-func (l *UserRegisterLogic) UserRegister(req *types.RegisterRequest) (resp *types.RegisterResponse, err error) {
+func (l *UserRegisterLogic) UserRegister(req *types.RegisterRequest) (resp *types.RegisterResponse, error error) {
 	resp = new(types.RegisterResponse)
 	//先查询用户名是否已注册
 	bl := l.svcCtx.RedisDB.SIsMember("username", req.UserName)
@@ -37,8 +39,22 @@ func (l *UserRegisterLogic) UserRegister(req *types.RegisterRequest) (resp *type
 		Username: req.UserName,
 		Password: req.PassWord,
 	})
+	if err != nil {
+		logx.Error("注册入库错误：", err)
+		return
+	}
+
+	//生成token
+	user := model.User{UserID: RgRes.UserId}
+	token, err := common.GenAccessToken(user)
+	if err != nil {
+		logx.Error("生成token错误：", err)
+		return
+	}
+
 	resp.StatusMsg = RgRes.StatusMsg
 	resp.StatusCode = RgRes.StatusCode
 	resp.UserId = RgRes.UserId
+	resp.Token = token
 	return resp, nil
 }
