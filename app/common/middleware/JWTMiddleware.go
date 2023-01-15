c8fc69c61417c4fc9ea00dd8504be6abc9f94cdb
lluckyboi <1598273095@qq.com>
2023-01-15 16:46:14 +0800

    update:resp

--- app/common/middleware/JWTMiddleware.go
+++ app/common/middleware/JWTMiddleware.go
@@ -23,7 +23,7 @@ func (m *JWTMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 		log.Println("Start JWT Auth")
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			resp := common.OKResp{
+			resp := common.Resp{
 				Status: 401,
 				Info:   "请求头中auth为空",
 			}
@@ -33,7 +33,7 @@ func (m *JWTMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 		// 按空格分割
 		parts := strings.SplitN(authHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			resp := common.OKResp{
+			resp := common.Resp{
 				Status: 401,
 				Info:   "请求头中auth格式有误",
 			}
@@ -43,7 +43,7 @@ func (m *JWTMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 		// parts[1]是获取到的tokenString，我们使用之前定义好的解析JWT的函数来解析它
 		mc, err := common.ParseToken(parts[1])
 		if err != nil {
-			resp := common.OKResp{
+			resp := common.Resp{
 				Status: 401,
 				Info:   "无效的Token",
 			}
