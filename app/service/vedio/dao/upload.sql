dc4098edf697c6b32e9ec1d7faea7edad51b4517
lluckyboi <1598273095@qq.com>
2023-01-18 18:07:08 +0800

    update:优化video.sql、upload.sql

--- app/service/vedio/dao/upload.sql
+++ app/service/vedio/dao/upload.sql
@@ -1,13 +1,14 @@
 # upload表
 create table upload
 (
-    upload_time datetime     not null on update CURRENT_TIMESTAMP comment '上次时间戳',
-    title       varchar(255) not null comment '标题',
-    user_id     bigint       not null comment '用户id',
-    video_id    bigint       not null comment '视频id',
+    upload_time datetime        not null on update CURRENT_TIMESTAMP comment '上次时间戳',
+    title       varchar(255)    not null comment '标题',
+    user_id     bigint unsigned not null comment '用户id',
+    video_id    bigint unsigned not null comment '视频id',
     constraint user_id
         foreign key (user_id) references user (user_id),
     constraint video
         foreign key (video_id) references video (video_id)
 );
+
 
--- app/service/vedio/dao/video.sql
+++ app/service/vedio/dao/video.sql
@@ -1,11 +1,11 @@
 # video表
 create table video
 (
-    video_id       bigint        not null comment 'vedio唯一标识'
+    video_id       bigint unsigned             not null comment 'video唯一标识'
         primary key,
-    play_url       varchar(255)  not null comment '播放地址',
-    cover_url      varchar(255)  not null comment '封面地址',
-    favorite_count int default 0 not null comment '点赞数',
-    comment_count  int           not null comment '评论数'
+    play_url       varchar(255)                not null comment '播放地址',
+    cover_url      varchar(255)                not null comment '封面地址',
+    favorite_count bigint unsigned default '0' not null comment '点赞数',
+    comment_count  bigint unsigned default '0' not null comment '评论数'
 );
 
