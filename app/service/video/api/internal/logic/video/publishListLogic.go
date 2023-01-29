7f6a7de8d48db29fc86964dc3b335f48c00b7176
lluckyboi <1598273095@qq.com>
2023-01-29 20:29:55 +0800

    feat(video):完成消息列表

--- app/service/video/api/internal/logic/video/publishListLogic.go
+++ app/service/video/api/internal/logic/video/publishListLogic.go
@@ -1,7 +1,12 @@
 package video
 
 import (
+	"SimpleDouYin/app/common/jwt"
+	"SimpleDouYin/app/common/key"
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/video/rpc/videosv"
 	"context"
+	"strconv"
 
 	"SimpleDouYin/app/service/video/api/internal/svc"
 	"SimpleDouYin/app/service/video/api/internal/types"
@@ -23,8 +28,64 @@ func NewPublishListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Publi
 	}
 }
 
-func (l *PublishListLogic) PublishList(req *types.PublishListRequest) (resp *types.PublishListResponse, err error) {
-	// todo: add your logic here and delete this line
+func (l *PublishListLogic) PublishList(req *types.PublishListRequest) (*types.PublishListResponse, error) {
+	resp := new(types.PublishListResponse)
+	//解析token
+	_, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = status.ErrFailParseToken
+		resp.StatusMsg = "token解析失败"
+		logx.Error(err.Error())
+		return resp, nil
+	}
+	//解析ID
+	uid, err := strconv.ParseInt(req.UserId, 10, 64)
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "UID有误或解析错误"
+		return resp, err
+	}
+	//查询id是否存在
+	bl := l.svcCtx.RedisDB.SIsMember(key.RedisUserIdCacheKey, uid)
+	if bl.Val() == false {
+		resp.StatusCode = status.ErrNoSuchUser
+		resp.StatusMsg = "无效的id"
+		return resp, nil
+	}
 
-	return
+	Grsp, err := l.svcCtx.VideoClient.PublishList(l.ctx, &videosv.PublishListReq{
+		UserId: uid,
+	})
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		logx.Error(err.Error())
+		return resp, nil
+	}
+
+	resp.StatusCode = Grsp.StatusCode
+	resp.StatusMsg = Grsp.StatusMsg
+
+	VideoList := Grsp.VideoList
+	for _, v := range VideoList {
+		author := types.Author{
+			Id:            v.Author.Id,
+			Name:          v.Author.Name,
+			FollowCount:   v.Author.FollowCount,
+			FollowerCount: v.Author.FollowerCount,
+			IsFollow:      v.Author.IsFollow,
+		}
+		video := types.Video{
+			Id:            v.Id,
+			Author:        author,
+			PlayUrl:       v.PlayUrl,
+			CoverUrl:      v.CoverUrl,
+			FavoriteCount: v.FavoriteCount,
+			CommentCount:  v.CommentCount,
+			IsFavorite:    v.IsFavorite,
+			Title:         v.Title,
+		}
+		resp.VideoList = append(resp.VideoList, video)
+	}
+	return resp, nil
 }
--- app/service/video/rpc/internal/logic/publishListLogic.go
+++ app/service/video/rpc/internal/logic/publishListLogic.go
@@ -1,10 +1,15 @@
 package logic
 
 import (
-	"context"
-
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/video/dao/model"
 	"SimpleDouYin/app/service/video/rpc/internal/svc"
 	"SimpleDouYin/app/service/video/rpc/pb"
+	"context"
+	"errors"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +29,99 @@ func NewPublishListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Publi
 }
 
 func (l *PublishListLogic) PublishList(in *pb.PublishListReq) (*pb.PublishListResp, error) {
-	// todo: add your logic here and delete this line
+	resp := new(pb.PublishListResp)
 
-	return &pb.PublishListResp{}, nil
+	//通过ID查询发布情况
+	var publishs []model.Publish
+	var pubCount int64
+	err := l.svcCtx.GormDB.Where("user_id = ?", in.UserId).
+		Find(&publishs).
+		Count(&pubCount)
+	if err.Error != nil && !errors.Is(err.Error, gorm.ErrRecordNotFound) {
+		log.Println("ID查询出错:", err.Error, " count:", pubCount)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	}
+	if pubCount == 0 {
+		resp.StatusCode = http.StatusOK
+		resp.StatusMsg = "成功"
+		return resp, nil
+	}
+	log.Println("publish查询成功,pubCount:", pubCount, "  publishes:", publishs)
+
+	//查询视频
+	var VIDS []int64
+	var videos []model.Video
+	for idx := 0; int64(idx) < pubCount; idx++ {
+		VIDS = append(VIDS, publishs[idx].VideoID)
+	}
+	errr := l.svcCtx.GormDB.Model(&model.Video{}).
+		Where("video_id in ?", VIDS).
+		Find(&videos)
+	if errr.Error != nil && (!errors.Is(errr.Error, gorm.ErrRecordNotFound)) {
+		log.Println("查询出错:", errr.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	}
+	log.Println("video查询成功:", videos)
+
+	//查询作者
+	users := make([]model.User, pubCount)
+	for idx := 0; int64(idx) < pubCount; idx++ {
+		errr = l.svcCtx.GormDB.Model(&model.User{}).
+			Where("user_id = ?", publishs[idx].UserID).
+			First(&users[idx])
+		if errr.Error != nil && (!errors.Is(errr.Error, gorm.ErrRecordNotFound)) {
+			log.Println("查询出错:", errr.Error)
+			resp.StatusCode = status.ErrOfServer
+			resp.StatusMsg = status.InfoErrOfServer
+			return resp, nil
+		}
+	}
+	log.Println("users查询成功:", users)
+
+	//关注情况
+	//user对应follow关系
+	isF := true
+	errr = l.svcCtx.GormDB.Where("uid = ? and target_uid = ?")
+	if errr.Error != nil && (!errors.Is(errr.Error, gorm.ErrRecordNotFound)) {
+		log.Println("查询出错:", errr.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	} else if errors.Is(errr.Error, gorm.ErrRecordNotFound) {
+		isF = false
+	}
+	log.Println("follow查询成功")
+
+	//整合结果
+	for i := 0; int64(i) < pubCount; i++ {
+		var Author pb.Author
+		var resVi pb.Video
+
+		//author
+		Author.Id = users[i].UserID
+		Author.FollowerCount = users[i].FollowCount
+		Author.FollowerCount = users[i].FollowerCount
+		Author.Name = users[i].Name
+		Author.IsFollow = isF
+
+		//video
+		resVi.Author = &Author
+		resVi.Title = publishs[i].Title
+		resVi.Id = publishs[i].VideoID
+		resVi.IsFavorite = false
+		resVi.CommentCount = videos[i].CommentCount
+		resVi.FavoriteCount = videos[i].FavoriteCount
+		resVi.CoverUrl = videos[i].CoverURL
+		resVi.PlayUrl = videos[i].PlayURL
+
+		resp.VideoList = append(resp.VideoList, &resVi)
+	}
+
+	resp.StatusCode = http.StatusOK
+	resp.StatusMsg = "成功"
+	return resp, nil
 }
