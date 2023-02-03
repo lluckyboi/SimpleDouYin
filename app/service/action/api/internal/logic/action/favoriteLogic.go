832470ca53ebe55343d1675365932baece97ce30
lluckyboi <1598273095@qq.com>
2023-02-03 16:06:42 +0800

    update(action.api):完善点赞逻辑

--- app/service/action/api/internal/logic/action/favoriteLogic.go
+++ app/service/action/api/internal/logic/action/favoriteLogic.go
@@ -1,10 +1,13 @@
 package action
 
 import (
-	"context"
-
+	"SimpleDouYin/app/common/jwt"
+	"SimpleDouYin/app/common/status"
 	"SimpleDouYin/app/service/action/api/internal/svc"
 	"SimpleDouYin/app/service/action/api/internal/types"
+	"SimpleDouYin/app/service/action/rpc/action"
+	"context"
+	"strconv"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -23,8 +26,36 @@ func NewFavoriteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Favorite
 	}
 }
 
-func (l *FavoriteLogic) Favorite(req *types.FavoriteReq) (resp *types.FavoriteResp, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+func (l *FavoriteLogic) Favorite(req *types.FavoriteReq) (*types.FavoriteResp, error) {
+	resp := new(types.FavoriteResp)
+	//解析token
+	claims, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = status.ErrFailParseToken
+		resp.StatusMsg = "token解析失败"
+		logx.Error(err.Error())
+		return resp, nil
+	}
+	//解析ID
+	vid, err := strconv.ParseInt(req.VideoId, 10, 64)
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "VID有误或服务器错误"
+		return resp, err
+	}
+	//rpc
+	Grsp, err := l.svcCtx.ActionClient.Favorite(l.ctx, &action.FavoriteReq{
+		UserId:     claims.UserId,
+		VideoId:    vid,
+		ActionType: false,
+	})
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		logx.Error(err.Error())
+		return resp, nil
+	}
+	resp.StatusCode = Grsp.StatusCode
+	resp.StatusMsg = Grsp.StatusMsg
+	return resp, nil
 }
