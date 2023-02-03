45159658b83137eab2113203587872dd7be4c230
lluckyboi <1598273095@qq.com>
2023-02-03 16:57:01 +0800

    feat(action):完成喜欢列表逻辑

--- app/service/action/api/internal/logic/action/favoriteListLogic.go
+++ app/service/action/api/internal/logic/action/favoriteListLogic.go
@@ -1,7 +1,11 @@
 package action
 
 import (
+	"SimpleDouYin/app/common/jwt"
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/action/rpc/action"
 	"context"
+	"strconv"
 
 	"SimpleDouYin/app/service/action/api/internal/svc"
 	"SimpleDouYin/app/service/action/api/internal/types"
@@ -23,7 +27,36 @@ func NewFavoriteListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Favo
 	}
 }
 
-func (l *FavoriteListLogic) FavoriteList(req *types.FavoriteListReq) (resp *types.FavoriteListResp, err error) {
+func (l *FavoriteListLogic) FavoriteList(req *types.FavoriteListReq) (*types.FavoriteListResp, error) {
+	resp := new(types.FavoriteListResp)
+	//解析token
+	_, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrFailParseToken)
+		resp.StatusMsg = "token解析失败"
+		logx.Error(err.Error())
+		return resp, nil
+	}
 
-	return
+	//解析ID
+	uid, err := strconv.ParseInt(req.UserId, 10, 64)
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrOfServer)
+		resp.StatusMsg = "UID有误或服务器错误"
+		return resp, err
+	}
+
+	//rpc
+	Grsp, err := l.svcCtx.ActionClient.FavoriteList(l.ctx, &action.FavoriteListReq{
+		UserId: uid,
+	})
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrOfServer)
+		resp.StatusMsg = status.InfoErrOfServer
+		logx.Error(err.Error())
+		return resp, nil
+	}
+	resp.StatusCode = strconv.Itoa(int(Grsp.StatusCode))
+	resp.StatusMsg = Grsp.StatusMsg
+	return resp, nil
 }
--- app/service/action/rpc/internal/logic/favoriteListLogic.go
+++ app/service/action/rpc/internal/logic/favoriteListLogic.go
@@ -1,10 +1,16 @@
 package logic
 
 import (
-	"context"
-
+	"SimpleDouYin/app/common/key"
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/common/tool"
+	"SimpleDouYin/app/dao/model"
 	"SimpleDouYin/app/service/action/rpc/internal/svc"
 	"SimpleDouYin/app/service/action/rpc/pb"
+	"context"
+	"errors"
+	"gorm.io/gorm"
+	"log"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +30,143 @@ func NewFavoriteListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Favo
 }
 
 func (l *FavoriteListLogic) FavoriteList(in *pb.FavoriteListReq) (*pb.FavoriteListResp, error) {
-	// todo: add your logic here and delete this line
+	resp := new(pb.FavoriteListResp)
 
-	return &pb.FavoriteListResp{}, nil
+	//查询点赞列表
+	var (
+		favorites []model.Favorite
+		favCt     int64
+	)
+	err := l.svcCtx.GormDB.Where("user_id = ?", in.UserId).
+		Find(&favorites).
+		Count(&favCt)
+	if err.Error != nil && !errors.Is(err.Error, gorm.ErrRecordNotFound) {
+		log.Println("ID查询出错:", err.Error, " count:", favCt)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	}
+	if favCt == 0 {
+		resp.StatusCode = status.SuccessCode
+		resp.StatusMsg = "成功"
+		return resp, nil
+	}
+
+	//查询视频
+	var VIDS []int64
+	var videos []model.Video
+	for idx := 0; int64(idx) < favCt; idx++ {
+		VIDS = append(VIDS, favorites[idx].VideoID)
+	}
+	errr := l.svcCtx.GormDB.Model(&model.Video{}).
+		Where("video_id in ?", VIDS).
+		Find(&videos)
+	if errr.Error != nil && (!errors.Is(errr.Error, gorm.ErrRecordNotFound)) {
+		log.Println("查询出错:", errr.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	}
+	log.Println("video查询成功:", videos)
+
+	//查询作者
+	var (
+		usersTp []model.User
+		UIDS    []int64
+		userCt  int64
+	)
+	for idx := 0; int64(idx) < favCt; idx++ {
+		UIDS = append(UIDS, favorites[idx].UserID)
+	}
+	errr = l.svcCtx.GormDB.Model(&model.User{}).
+		Where("user_id in ?", UIDS).
+		Order(tool.FiledStringBuild("user_id", UIDS)).
+		Find(&usersTp).
+		Count(&userCt)
+	if errr.Error != nil {
+		log.Println("作者查询出错:", errr.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "服务器错误"
+		return resp, nil
+	}
+	log.Println("作者查询成功:", usersTp)
+	//补全
+	UserIdx := make(map[int64]int)
+	for _, val := range favorites {
+		for idx, uv := range usersTp {
+			if val.UserID == uv.UserID {
+				UserIdx[uv.UserID] = idx
+			}
+		}
+	}
+	var users []model.User
+	for i := 0; int64(i) < favCt; i++ {
+		users = append(users, usersTp[UserIdx[favorites[i].UserID]])
+	}
+	log.Println("users补全成功:", users)
+
+	//user对应follow关系
+	var Tfollows []model.Follow
+	follows := make([]bool, favCt)
+	for i := 0; int64(i) < favCt; i++ {
+		follows[i] = false
+	}
+
+	errr1 := l.svcCtx.GormDB.
+		Where("uid = ? and target_uid in ?", in.UserId, UIDS).
+		Find(&Tfollows)
+	if errr1.Error != nil && (!errors.Is(errr1.Error, gorm.ErrRecordNotFound)) {
+		log.Println("查询出错:", errr1.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "服务器错误"
+		return resp, nil
+	}
+	for _, v := range Tfollows {
+		for ku, vu := range UIDS {
+			if vu == v.TargetUID {
+				follows[ku] = true
+			}
+		}
+	}
+	log.Println("follows查询成功:", follows)
+
+	//title查询
+	var publishs []model.Publish
+	errr = l.svcCtx.GormDB.Model(&model.Publish{}).
+		Where("user_id = ? and video_id in ?", in.UserId, VIDS).
+		Order(tool.FiledStringBuild("video_id", VIDS)).
+		Limit(key.FeedNum).
+		Find(&publishs)
+	if errr.Error != nil && (!errors.Is(errr.Error, gorm.ErrRecordNotFound)) {
+		log.Println("查publish表错误", errr.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "服务器错误"
+		return resp, nil
+	}
+
+	//整合成结果
+	for i := 0; int64(i) < favCt; i++ {
+		var Author pb.Author
+		var resVi pb.Video
+
+		//author
+		Author.Id = users[i].UserID
+		Author.FollowerCount = users[i].FollowCount
+		Author.FollowerCount = users[i].FollowerCount
+		Author.Name = users[i].Name
+		Author.IsFollow = follows[i]
+
+		//video
+		resVi.Author = &Author
+		resVi.Title = publishs[i].Title
+		resVi.Id = favorites[i].VideoID
+		resVi.IsFavorite = true
+		resVi.CommentCount = videos[i].CommentCount
+		resVi.FavoriteCount = videos[i].FavoriteCount
+		resVi.CoverUrl = videos[i].CoverURL
+		resVi.PlayUrl = videos[i].PlayURL
+
+		resp.VideoList = append(resp.VideoList, &resVi)
+	}
+	return resp, nil
 }
