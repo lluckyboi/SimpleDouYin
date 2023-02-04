f970322fec0ab2d51be568f508d49d87e8993347
lluckyboi <1598273095@qq.com>
2023-02-04 18:28:43 +0800

    feat:发送消息

--- app/service/chat/api/internal/logic/chat/sendMsgLogic.go
+++ app/service/chat/api/internal/logic/chat/sendMsgLogic.go
@@ -1,7 +1,13 @@
 package chat
 
 import (
+	"SimpleDouYin/app/common/jwt"
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/common/tool"
+	"SimpleDouYin/app/service/chat/rpc/chat"
 	"context"
+	"log"
+	"strconv"
 
 	"SimpleDouYin/app/service/chat/api/internal/svc"
 	"SimpleDouYin/app/service/chat/api/internal/types"
@@ -23,8 +29,47 @@ func NewSendMsgLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SendMsgLo
 	}
 }
 
-func (l *SendMsgLogic) SendMsg(req *types.SendMsgReq) (resp *types.SendMsgResp, err error) {
-	// todo: add your logic here and delete this line
+func (l *SendMsgLogic) SendMsg(req *types.SendMsgReq) (*types.SendMsgResp, error) {
+	resp := new(types.SendMsgResp)
 
-	return
+	//解析token
+	claims, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = status.ErrFailParseToken
+		resp.StatusMsg = "token解析失败"
+		logx.Error(err.Error())
+		return resp, nil
+	}
+
+	//解析ID
+	tuid, err := strconv.ParseInt(req.ToUserId, 10, 64)
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = "VID有误或服务器错误"
+		return resp, nil
+	}
+
+	//校验ActionType 1-关注-true 2-取消-false
+	act, err := tool.AcTypeStringToBool(req.ActionType)
+	if err != nil {
+		resp.StatusCode = status.ErrUnknownAcType
+		resp.StatusMsg = "unknown ActionType"
+		return resp, nil
+	}
+
+	Grsp, err := l.svcCtx.ChatClient.SendMsg(l.ctx, &chat.SendMsgReq{
+		UserId:       claims.UserId,
+		TargetUserId: tuid,
+		ActionType:   act,
+		Content:      req.Content,
+	})
+	if err != nil {
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		log.Print("关注rpc错误", err.Error())
+		return resp, nil
+	}
+	resp.StatusCode = Grsp.StatusCode
+	resp.StatusMsg = Grsp.StatusMsg
+	return resp, nil
 }
--- app/service/chat/rpc/internal/logic/sendMsgLogic.go
+++ app/service/chat/rpc/internal/logic/sendMsgLogic.go
@@ -1,7 +1,10 @@
 package logic
 
 import (
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/chat/dao/model"
 	"context"
+	"time"
 
 	"SimpleDouYin/app/service/chat/rpc/internal/svc"
 	"SimpleDouYin/app/service/chat/rpc/pb"
@@ -24,7 +27,24 @@ func NewSendMsgLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SendMsgLo
 }
 
 func (l *SendMsgLogic) SendMsg(in *pb.SendMsgReq) (*pb.SendMsgResp, error) {
-	// todo: add your logic here and delete this line
+	resp := new(pb.SendMsgResp)
 
-	return &pb.SendMsgResp{}, nil
+	//构造入库
+	msg := &model.Message{
+		Content:    in.Content,
+		CreateTime: time.Now(),
+		UID:        in.UserId,
+		TargetUID:  in.TargetUserId,
+	}
+	rst := l.svcCtx.GormDB.Create(msg)
+	if rst.Error != nil {
+		logx.Info("创建message失败", rst.Error)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, rst.Error
+	}
+
+	resp.StatusCode = status.SuccessCode
+	resp.StatusMsg = "发送成功"
+	return resp, nil
 }
