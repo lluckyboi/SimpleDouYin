5d16de3acb9957dccdbce5223f8257cb8021f5cb
lluckyboi <1598273095@qq.com>
2023-02-04 18:45:26 +0800

    feat:聊天记录

--- app/service/chat/api/internal/logic/chat/msgRecordLogic.go
+++ app/service/chat/api/internal/logic/chat/msgRecordLogic.go
@@ -1,7 +1,12 @@
 package chat
 
 import (
+	"SimpleDouYin/app/common/jwt"
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/chat/rpc/chat"
 	"context"
+	"log"
+	"strconv"
 
 	"SimpleDouYin/app/service/chat/api/internal/svc"
 	"SimpleDouYin/app/service/chat/api/internal/types"
@@ -24,7 +29,46 @@ func NewMsgRecordLogic(ctx context.Context, svcCtx *svc.ServiceContext) *MsgReco
 }
 
 func (l *MsgRecordLogic) MsgRecord(req *types.MsgRecordReq) (resp *types.MsgRecordResp, err error) {
-	// todo: add your logic here and delete this line
+	//解析token
+	claims, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrFailParseToken)
+		resp.StatusMsg = "token解析失败"
+		logx.Error(err.Error())
+		return resp, nil
+	}
 
-	return
+	//解析ID
+	tuid, err := strconv.ParseInt(req.ToUserId, 10, 64)
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrOfServer)
+		resp.StatusMsg = "ID有误或服务器错误"
+		return resp, nil
+	}
+
+	//调rpc
+	Grsp, err := l.svcCtx.ChatClient.MsgRecord(l.ctx, &chat.MsgRecordReq{
+		UserId:       claims.UserId,
+		TargetUserId: tuid,
+	})
+	if err != nil {
+		resp.StatusCode = strconv.Itoa(status.ErrOfServer)
+		resp.StatusMsg = status.InfoErrOfServer
+		log.Print("关注rpc错误", err.Error())
+		return resp, nil
+	}
+	resp.StatusCode = strconv.Itoa(int(Grsp.StatusCode))
+	resp.StatusMsg = Grsp.StatusMsg
+
+	//msg list
+	msgList := Grsp.MsgList
+	for _, v := range msgList {
+		msg := types.Message{
+			Id:         v.Id,
+			Content:    v.Content,
+			CreateTime: v.CreatTime,
+		}
+		resp.MsgList = append(resp.MsgList, msg)
+	}
+	return resp, nil
 }
--- app/service/chat/rpc/internal/logic/msgRecordLogic.go
+++ app/service/chat/rpc/internal/logic/msgRecordLogic.go
@@ -1,7 +1,12 @@
 package logic
 
 import (
+	"SimpleDouYin/app/common/status"
+	"SimpleDouYin/app/service/chat/dao/model"
 	"context"
+	"errors"
+	"gorm.io/gorm"
+	"log"
 
 	"SimpleDouYin/app/service/chat/rpc/internal/svc"
 	"SimpleDouYin/app/service/chat/rpc/pb"
@@ -24,7 +29,34 @@ func NewMsgRecordLogic(ctx context.Context, svcCtx *svc.ServiceContext) *MsgReco
 }
 
 func (l *MsgRecordLogic) MsgRecord(in *pb.MsgRecordReq) (*pb.MsgRecordResp, error) {
-	// todo: add your logic here and delete this line
+	resp := new(pb.MsgRecordResp)
 
-	return &pb.MsgRecordResp{}, nil
+	var (
+		msgs  []model.Message
+		msgCt int64
+	)
+	rst := l.svcCtx.GormDB.Where("(uid = ? and target_uid = ?)||(target_uid = ? and uid = ?)",
+		in.UserId, in.TargetUserId, in.TargetUserId, in.UserId).
+		Order("create_time desc").
+		Find(&msgs).
+		Count(&msgCt)
+	if rst.Error != nil && !errors.Is(rst.Error, gorm.ErrRecordNotFound) {
+		log.Println("聊天记录查询出错:", rst.Error, " count:", msgCt)
+		resp.StatusCode = status.ErrOfServer
+		resp.StatusMsg = status.InfoErrOfServer
+		return resp, nil
+	}
+
+	//整合结果
+	for i := 0; int64(i) < msgCt; i++ {
+		msg := &pb.Msg{
+			Id:        msgs[i].ID,
+			Content:   msgs[i].Content,
+			CreatTime: msgs[i].CreateTime.Format("2006-01-02 15-01-05"),
+		}
+		resp.MsgList = append(resp.MsgList, msg)
+	}
+	resp.StatusCode = status.SuccessCode
+	resp.StatusMsg = "获取聊天记录成功"
+	return resp, nil
 }
